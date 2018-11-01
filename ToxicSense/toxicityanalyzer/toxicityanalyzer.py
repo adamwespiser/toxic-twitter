4a52b866056f4c279dfe142cc8b8feaae7f1cff0
adamwespiser <adamwespiser@gmail.com>
2018-11-01 00:43:26 -0400

    char-cnn model for tweet toxicity is working

--- ToxicSense/toxicityanalyzer/toxicityanalyzer.py
+++ ToxicSense/toxicityanalyzer/toxicityanalyzer.py
@@ -1,46 +1,46 @@
 import random
-from django.apps import AppConfig
-from django.apps import apps
 import numpy as np
 import unidecode
 import logging
-import os
+import sys, os, re, csv, codecs
+import numpy as np
+import unidecode
+import json
 
-import sys, os, re, csv, codecs, numpy as np, pandas as pd
-import matplotlib.pyplot as plt
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation, GRU,Conv1D,MaxPooling1D
-from keras.layers import Bidirectional, GlobalMaxPool1D,Bidirectional
-from keras.models import Model
-from keras import initializers, regularizers, constraints, optimizers, layers
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.models import model_from_json
+from keras import backend
+import keras.backend
+from keras.backend.tensorflow_backend import set_session
 import gc
-from sklearn.model_selection import train_test_split
 from keras.models import load_model
 import tensorflow as tf
-import tensorflowjs as tfjs
-from keras.models import model_from_json
-import unidecode
-import keras.backend
-import json
-import tensorflow as tf
-from keras.backend.tensorflow_backend import set_session
-
-
-
 
 
 logger = logging.getLogger(__name__)
+
+config = tf.ConfigProto(
+# set tensorflow to run on only the CPU
+# https://stackoverflow.com/questions/37660312/how-to-run-tensorflow-on-cpu
+        device_count = {'GPU': 0}
+)
+config.gpu_options.allow_growth = True
+set_session(tf.Session(config=config))
+
+
 global loaded_model
 global char_map
+
 loaded_model = None
 char_map = None
-path = os.path.dirname(os.path.realpath(__file__)) + "/resources/"
 
+# Set our model resources, and character -> int map
+path = os.path.dirname(os.path.realpath(__file__)) + "/resources/"
 model_json_file = path + 'model-ascii.json'
 model_h5_file = path + "model-ascii.h5"
 char_to_index = path + "ascii-char-map.json"
+
 def read_in_keras_model(mjson_file, weights_file, char_file):
     json_file = open(mjson_file, 'r')
     loaded_model_json = json_file.read()
@@ -49,7 +49,9 @@ def read_in_keras_model(mjson_file, weights_file, char_file):
     loaded_model.load_weights(weights_file)
     char_map = char_map = json.loads(open(char_file).read())
     return loaded_model, char_map
-loaded_model, char_map = read_in_keras_model(model_json_file, model_h5_file,char_to_index)
+loaded_model, char_map = read_in_keras_model(model_json_file,
+                                             model_h5_file,
+                                             char_to_index)
 
 labels = ["toxic",
           "severe_toxic",
@@ -58,25 +60,31 @@ labels = ["toxic",
           "insult",
           "identity_hate"]
 
+def format_tweet(tweet, char_map):
+    default_char = 1 # corresponds to ' ' (space)
+    tweet_array = list(unidecode.unidecode(tweet.lower()))
+    tweet_int = [char_map.get(x,1) for x in tweet_array]
+    # the model (as is) only works for 500 char arrays
+    if (len(tweet_int) < 500):
+        tweet_int = list([0]*(500 - len(tweet_int))) + tweet_int
+    logger.warn(tweet_int)
+    array = np.array(tweet_int).astype(int).reshape(1,500)
+    return array
 
 
 def get_toxicity(text):
     # char_map is a set global var
+    logger.warn(text)
+    # if the text is longer than 500, just look at the end
+    if len(text) > 500:
+        text = text[(len(text)-500):len(text)]
     global char_map
     global loaded_model
-
     tweet_input = format_tweet(text, char_map)
-    pred = loaded_model.predict(tweet_input)
-    output = pred.tolist()
-    return max(output)
+    # bug: https://github.com/keras-team/keras/issues/2397
+    with backend.get_session().graph.as_default() as g:
+        model = loaded_model
+        pred = loaded_model.predict(tweet_input)
+        output = np.max(pred)
+        return float(output)
 
-def format_tweet(tweet, char_map):
-    tweet_array = list(unidecode.unidecode(tweet.lower()))
-    if len(tweet_array) > 500:
-        tweet_array = tweet_array[0:500]
-    tweet_int = [char_map.get(x) for x in tweet_array]
-    if (len(tweet_int) < 500):
-        tweet_int = list([0]*(500 - len(tweet_int))) + tweet_int
-
-    array = np.array(tweet_int).astype(int).reshape(1,500)
-    return array
