396af336db20dcb5ff756783725dbfd48806ab11
Mukund Muralikrishnan <mukund@healthifyme.com>
2018-12-12 07:49:09 +0530

    Fixes README. Better prints in code

--- create-model/make_prediction.py
+++ create-model/make_prediction.py
@@ -1,24 +1,23 @@
-import sys, numpy as np
+import sys
 import gc
-import unidecode
 import json
 
+import numpy as np
 import tensorflow as tf
+import unidecode
 
 from keras.backend.tensorflow_backend import set_session
 from keras.models import load_model
 from keras.models import model_from_json
 
 
-config = tf.ConfigProto(
-    device_count = {'GPU': 0}
-)
+model_json_file = 'ascii-3-model/model-ascii.json'
+model_h5_file = 'ascii-3-model/model-ascii.h5'
+char_to_index = 'ascii-3-model/ascii-char-map.json'
+
+config = tf.ConfigProto(device_count={'GPU': 0})
 config.gpu_options.allow_growth = True
 set_session(tf.Session(config=config))
-
-model_json_file = 'ascii-3-model/model-ascii.json'
-model_h5_file = "ascii-3-model/model-ascii.h5"
-char_to_index = "ascii-3-model/ascii-char-map.json"
 
 
 def read_in_keras_model(mjson_file, weights_file):
@@ -30,10 +29,11 @@ def read_in_keras_model(mjson_file, weights_file):
     char_map = char_map = json.loads(open(char_to_index).read())
     return loaded_model, char_map
 
+
 def format_tweet(tweet, char_map):
     tweet_array = list(unidecode.unidecode(tweet.lower()))
     tweet_int = [char_map.get(x) for x in tweet_array]
-    if (len(tweet_int) < 500):
+    if len(tweet_int) < 500:
         tweet_int = list([0]*(500 - len(tweet_int))) + tweet_int
 
     array = np.array(tweet_int).astype(int).reshape(1,500)
@@ -41,23 +41,23 @@ def format_tweet(tweet, char_map):
 
 
 def run_main():
-    print(len(sys.argv))
     if len(sys.argv) > 1:
         tweet = sys.argv[1]
+        print('Tweet:')
         print(tweet)
+        print()
     else:
         sys.exit(0)
-        print("please provide a tweet as arg 1")
+        print('Please provide a tweet as arg 1')
     loaded_model, char_map = read_in_keras_model(model_json_file, model_h5_file)
     tweet_input = format_tweet(tweet, char_map)
 
-    labels = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+    labels = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
     pred = loaded_model.predict(tweet_input)
-    print(pred.tolist())
+    print('Predictions:')
     for i, p in enumerate(pred[0]):
-        print(f'{labels[i]}: {p}')
+        print('{}: {}'.format(labels[i], p))
 
 
 if __name__ == '__main__':
      run_main()
-
